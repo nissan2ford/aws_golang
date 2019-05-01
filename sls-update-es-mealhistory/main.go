1878940a225254557b6f477023d1b220a7fac37b
nissan2ford <nissan2ford-reg1@yahoo.co.jp>
2019-05-01 16:03:18 +0900

    20190501 added update-es

--- sls-update-es-mealhistory/main.go
+++ sls-update-es-mealhistory/main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
-	"fmt"
+//	"fmt"
 
-//	"encoding/json"
+	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/lambda"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+//	"github.com/aws/aws-sdk-go/service/dynamodb"
+//	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 
 
-//	"gopkg.in/olivere/elastic.v3"
-//	"github.com/edoardo849/apex-aws-signer"
+	"log"
+	"net/http"
+	"github.com/aws/aws-sdk-go/service/elasticsearchservice"
+	"gopkg.in/olivere/elastic.v3"
+	"github.com/edoardo849/apex-aws-signer"
 )
 
 // define elasticsearch setting
@@ -23,42 +26,81 @@ const (
 	ddbTablename = "meal_history"
 
 	esUrl = "https://search-es-mealhistory-yqcvq335bq5okinup2ohti24gy.ap-northeast-1.es.amazonaws.com"
-//	esIndex = 
+	esIndex = "mhindex"  // Index name
+	esType = "mealhistory" // Index group name
+
+	// AWS Region
+	region = "ap-northeast-1"
 )
 
 // define mealhistory type
-type MDdata struct {
+type MHdata struct {
 	MealTime	string	`json:"MealTime"`	// primary pertition key
 	Date		int	`json:"Date"`		// primary sort key
 	Day_of_week	string	`json:"Day_of_week"`
 	MealMethod	string	`json:"MealMethod"`
 }
 
-type Response struct {
-    Message string `json:"message"`
-    Ok      bool   `json:"ok"`
-}
-
-
-//func PutES()(Response, error) {
-//		transport := signer.NewTransport(session.New(&aws.Config{Region:aws.String(rec.AWSRegion)}), elasticsearchservice.ServiceName)
-
-//		httpClient := &http.Client{
-//			Transport: transport,
-//		}
-
-		// Elasticsearchに接続
-//		client, err := elastic.NewClient(
-//			elastic.SetSniff(false),
-//			elastic.SetURL(esUrl), // AWS Elasticsearchのエンドポイント
-//			elastic.SetScheme("https"),
-//			elastic.SetHttpClient(httpClient),
-//		)
+//type Response struct {
+//    Message string `json:"message"`
+//    Ok      bool   `json:"ok"`
 //}
 
-func Handler() (Response, error) {
+
+func PutES(mhdata *MHdata)(string, error) {
+		transport := signer.NewTransport(session.New(&aws.Config{Region:aws.String(region)}), elasticsearchservice.ServiceName)
+
+		httpClient := &http.Client{
+			Transport: transport,
+		}
+
+		// Elasticsearchに接続
+		client, err := elastic.NewClient(
+			elastic.SetSniff(false),
+			elastic.SetURL(esUrl), // AWS Elasticsearchのエンドポイント
+			elastic.SetScheme("https"),
+			elastic.SetHttpClient(httpClient),
+		)
+
+		if err != nil {
+			panic(err)
+		}
+
+		// Create an index.
+		indexName := esIndex
+
+		// Index a recode
+		msg, err := putDataIntoES(client,indexName,mhdata)
+
+	return msg, nil
+}
+
+// Index a recode of MHdata
+func putDataIntoES(c *elastic.Client, indexName string,mhdata *MHdata)(string, error) {
+
+	log.Printf("mhdata: %s\n", mhdata)
+
+	putdata, err := c.Index().
+		Index(indexName).
+		Type(esType).
+		BodyJson(mhdata).
+		Do()
+
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Indexed mhdata %s to index %s, type %s\n",putdata.Id,putdata.Index, putdata.Type)
+
+	// Convert JSON
+	b, err := json.Marshal(mhdata)
+
+	return string(b), nil
+}
+
+func Handler(input *MHdata) (string, error) {
 	// 処理部分
-	msg, err := Scanmealhistory()
+	msg, err := PutES(input)
 
 	return msg,err
 }
