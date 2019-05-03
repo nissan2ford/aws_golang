2ee1c512239cc41ee295c1d9efe943a040820e34
nissan2ford <nissan2ford-reg1@yahoo.co.jp>
2019-05-03 09:19:55 +0900

    20190503 update elasticsearch was completed

--- sls-update-es-mealhistory/main.go
+++ sls-update-es-mealhistory/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-//	"fmt"
+	"fmt"
 
 	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/lambda"
 
+	"github.com/aws/aws-lambda-go/events"
+	"context"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-//	"github.com/aws/aws-sdk-go/service/dynamodb"
-//	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 
 
 	"log"
@@ -96,18 +99,58 @@ func putDataIntoES(c *elastic.Client, indexName string,mhdata *MHdata)(string, e
 	return string(b), nil
 }
 
-func Handler(input *MHdata) (string, error) {
-	// 処理部分
-	msg, err := PutES(input)
+func UnmarshalStreamImage(attribute map[string]events.DynamoDBAttributeValue, out interface{}) error {
+
+	dbAttrMap := make(map[string]*dynamodb.AttributeValue)
+
+	for k, v := range attribute {
+
+		var dbAttr dynamodb.AttributeValue
+
+		bytes, marshalErr := v.MarshalJSON(); if marshalErr != nil {
+			return marshalErr
+		}
+
+		json.Unmarshal(bytes, &dbAttr)
+		dbAttrMap[k] = &dbAttr
+	}
+
+	return dynamodbattribute.UnmarshalMap(dbAttrMap, out)
+}
+
+func handleRequest(ctx context.Context, e events.DynamoDBEvent) (string, error) {
+
+	// initialize var
+	mhdata := MHdata{}
+
+	for _, record := range e.Records {
+		fmt.Printf("Processing request data for event ID %s, type %s.\n", record.EventID, record.EventName)
+
+		// Print new values for attributes of type String
+		for name, value := range record.Change.NewImage {
+			if value.DataType() == events.DataTypeString {
+				fmt.Printf("Attribute name: %s, value: %s\n", name, value.String())
+
+			}
+			
+			err := UnmarshalStreamImage(record.Change.NewImage, &mhdata)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	// Put elasticsearch
+	msg, err := PutES(&mhdata)
 
 	if err != nil {
 		panic(err)
 	}
 
 	return msg,err
+
 }
 
-
 func main() {
-    lambda.Start(Handler)
+    lambda.Start(handleRequest)
 }
