1acf9e563209a311532684d3609e63620782409c
nissan2ford <nissan2ford-reg1@yahoo.co.jp>
2019-07-15 16:06:22 +0900

    20190715 use os environmet var

--- sls-httping/main.go
+++ sls-httping/main.go
@@ -6,22 +6,35 @@ import (
 	"strconv"
 	"time"
 
+	"os"
+
 	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
-type Response struct {
-	ReqURL   string        `json:"requrl"`
-	Response string        `json:"response"`
-	Duration time.Duration `json:"duration"`
-	DoTime   int           `json:"dotime"`
+type dynamoData struct {
+	ReqURL   string `dynamo:"Req_URL"`
+	Response string `dynamo:"Res_Code"`
+	Duration int64  `dynamo:"Duration"`
+	DoTime   int    `dynamo:"Do_Time"`
 }
 
-func Handler() (Response, error) {
+// define dynamoDB setting
+const (
+	// dynamoDB Table Name
+	hcTablename = "IL_hc"
+
+	// AWS Region
+	region = "ap-northeast-1"
+)
+
+func Handler() (dynamoData, error) {
 
 	urls := []string{
-		//		"http://192.168.11.1",
-		"http://www.intellilink.co.jp",
-		//		"http://github.com",
+		os.Getenv("RequestURL"),
 	}
 
 	// make channel
@@ -62,12 +75,52 @@ func Handler() (Response, error) {
 	const layout = "20060102150405"
 	dotimeint, _ := strconv.Atoi(dotime.Format(layout))
 
-	return Response{
+	duratimeint := int64(duratime)
+
+	// set dynamoData
+	putdata := dynamoData{
 		ReqURL:   requrlstr,
 		Response: respstr,
-		Duration: duratime,
+		Duration: duratimeint,
 		DoTime:   dotimeint,
-	}, nil
+	}
+
+	// session
+	sess, err := session.NewSession()
+	if err != nil {
+		panic(err)
+	}
+
+	svc := dynamodb.New(sess, aws.NewConfig().WithRegion(region))
+
+	// パラメータ
+	params := &dynamodb.PutItemInput{
+		TableName: aws.String(hcTablename), // table name
+		Item: map[string]*dynamodb.AttributeValue{ // Input Datas
+			"Req_URL": {
+				S: aws.String(putdata.ReqURL),
+			},
+			"Res_Code": {
+				S: aws.String(putdata.Response),
+			},
+			"Duration": {
+				N: aws.String(strconv.FormatInt(putdata.Duration, 10)), // int64 -> string
+			},
+			"Do_Time": {
+				N: aws.String(strconv.Itoa(putdata.DoTime)),
+			},
+		},
+	}
+
+	// PutItemの実行
+	resp, err := svc.PutItem(params)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(resp)
+
+	return putdata, nil
 }
 
 func main() {
